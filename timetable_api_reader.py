6b6414d7107df9efe46a9d8406f74e0896916353
XxMrTyronexX <80863499+XxMrTyronexX@users.noreply.github.com>
2023-06-16 01:39:04 +0930

    Added and formatted API to get service alerts for .json

--- timetable_api_reader.py
+++ timetable_api_reader.py
@@ -6,94 +6,151 @@ import linecache
 import io
 
 #putting URL containing bus data inside of requests object
-r = requests.get("https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions/debug")
+r1 = requests.get("https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions/debug")
+r2 = requests.get("https://gtfs.adelaidemetro.com.au/v1/realtime/service_alerts/debug")
 
 #extracting the bus timetable data from the object as a string
-response = r.text
+response1 = r1.text
+response2 = r2.text
 
 #getting the lines of the string returned
-lines_of_response = io.StringIO(response).readlines()
+lines_of_response1 = io.StringIO(response1).readlines()
+lines_of_response2 = io.StringIO(response2).readlines()
 
 #creating blank variables to be concatinated or iterated on
-full_text = ""
+full_text1 = ""
+full_text2 = ""
 i = 1
+j = 1
+k = 1
 
 #looping through each line of the bus schedule
-for line in lines_of_response:
+for line in lines_of_response1:
 
     #all of the keys (entity) are the same, so this will number them
     if(line == "entity {\n"):
-        full_text = full_text + line.replace("entity {\n", "entity" + str(i) + " {\n")
+        full_text1 = full_text1 + line.replace("entity {\n", "entity" + str(i) + " {\n")
         i = i + 1
         
     else:
-        full_text = full_text + line
+        full_text1 = full_text1 + line
+
+#looping through each line of the service alerts
+for line in lines_of_response2:
+
+    if(line == "    informed_entity {\n"):
+        full_text2 = full_text2 + line.replace('    informed_entity {\n', '    "informed_entity' + str(j) + '": {\n')
+        j = j + 1
+        
+    elif(line == "entity {\n"):
+        full_text2 = full_text2 + line.replace('entity {\n', '"entity' + str(k) + '": {\n')
+        k = k + 1
+        
+    else:
+        full_text2 = full_text2 + line
 
 # Replacing all of the " with nothing (we will add them back
-full_text = full_text.replace('"', '')
+full_text1 = full_text1.replace('"', '')\
 
 #removing the square brackets (they are in the wrong spot)
-full_text = full_text.replace('[','')
-full_text = full_text.replace(']','')
+full_text1 = full_text1.replace('[','')
+full_text1 = full_text1.replace(']','')
+full_text2 = full_text2.replace('[','')
+full_text2 = full_text2.replace(']','')
 
 # Making response s2 so it is easy to keep track of each change I made
-s2 = full_text
+vehicle_positions2 = full_text1
+service_alerts2 = "{" + full_text2 + "}"
 
 # Surrounding any word with "
-s3 = re.sub('(\w+)', '"\g<1>"', s2)
+vehicle_positions3 = re.sub('(\w+)', '"\g<1>"', vehicle_positions2)\
 
 # Replacing "." with . because s3 also surrounds . with quotes
-s4 = re.sub('"\."', '.', s3)
+vehicle_positions4 = re.sub('"\."', '.', vehicle_positions3)
 
 # Adding : between " { because some keys do not have a colan
-s5 = re.sub('" {', '": {', s4)
+vehicle_positions5 = re.sub('" {', '": {', vehicle_positions4)
 
 # Adding curly brackets at the start and the end of the schedule and a name for it so we can loop through it
-s6 = '{' + s5 + '}'
+vehicle_positions6 = '{' + vehicle_positions5 + '}'
 
 # Adding a comma after each line ending with a "
-s7 = re.sub('"\n', '",\n', s6)
+vehicle_positions7 = re.sub('"\n', '",\n', vehicle_positions6)
 
 # Removing any extraneous white spaces
-s8 = re.sub('\s', '', s7)
+vehicle_positions8 = re.sub('\s', '', vehicle_positions7)
 
 # Removing the commas which were added in the wrong spot
-s9 = s8.replace('",}', '"}')
+vehicle_positions9 = vehicle_positions8.replace('",}', '"}')
 
 # Adding a coma at the end of each curly bracket that comes before a key
-s10 = s9.replace('"}"', '"},"')
+vehicle_positions10 = vehicle_positions9.replace('"}"', '"},"')
 
 # Moving " to go before the - in a negative number
-s11 = s10.replace(':-"', ':"-')
+vehicle_positions11 = vehicle_positions10.replace(':-"', ':"-')
 
 # Adding a comma after a curly bracket that is before a key but after another bracket
-s12 = s11.replace('}"', '},"')
-    
-#print(s12)
+vehicle_positions12 = vehicle_positions11.replace('}"', '},"')
+
+#formatting the service alerts return value so it can be read in json format
+service_alerts3 = re.sub('header {', '"header": {', service_alerts2)
+service_alerts4 = re.sub('      route_id: ', '      "route_id": ', service_alerts3)
+service_alerts5 = re.sub('  gtfs_realtime_version: ', '  "gtfs_realtime_version": ', service_alerts4)
+service_alerts6 = re.sub('  incrementality: ', '  "incrementality": "', service_alerts5)
+service_alerts7 = re.sub('  id: ', '  "id": ', service_alerts6)
+service_alerts8 = re.sub('  timestamp: ', '  "timestamp": "', service_alerts7)
+service_alerts9 = re.sub('    active_period {', '    "active_period": {', service_alerts8)
+service_alerts10 = re.sub('  alert {', '  "alert": {', service_alerts9)
+service_alerts11 = re.sub('      start: ', '      "start": "', service_alerts10)
+service_alerts12 = re.sub('    cause: ', '    "cause": "', service_alerts11)
+service_alerts13 = re.sub('    effect: ', '    "effect": "', service_alerts12)
+service_alerts14 = re.sub('    url {', '    "url": {', service_alerts13)
+service_alerts15 = re.sub('      translation {', '       "translation": {', service_alerts14)
+service_alerts16 = re.sub('        text: ', '        "text": ', service_alerts15)
+service_alerts17 = re.sub('    header_text {', '    "header_text": {', service_alerts16)
+service_alerts18 = re.sub('        language: ', '        "language": ', service_alerts17)
+service_alerts19 = re.sub('    description_text {', '    "description_text": {', service_alerts18)
+
+service_alerts20 = re.sub('\n.*?}"', '"\n"' , service_alerts19, flags=re.DOTALL)
+service_alerts21 = re.sub('\n', '",\n' , service_alerts20)
+service_alerts22 = re.sub(' {",', ' {' , service_alerts21)
+service_alerts23 = re.sub('"",\n','",\n' , service_alerts22)
+service_alerts24 = service_alerts23.replace(': {",\n', ': {\n')
+service_alerts25 = service_alerts24.replace('",\n}",', '"\n},')
+service_alerts26 = service_alerts25.replace('",\n }",', '"\n },')
+service_alerts27 = service_alerts26.replace('",\n  }",', '"\n  },')
+service_alerts28 = service_alerts27.replace('",\n   }",', '"\n   },')
+service_alerts29 = service_alerts28.replace('",\n    }",', '"\n    },')
+service_alerts30 = service_alerts29.replace('",\n     }",', '"\n    },')
+service_alerts31 = service_alerts30.replace(',\n      }"', '\n      }')
+service_alerts32 = service_alerts31.replace('      }\n    },\n  }"', '      }\n    }\n  }')
+service_alerts33 = re.sub('\<.*?>', '' , service_alerts32, flags=re.DOTALL)
+service_alerts34 = re.sub('      end: ', '      "end": "', service_alerts33)
+service_alerts35 = re.sub('},\n}', '}\n}', service_alerts34)
 
 #converting our reformatted string into a json file (we can read it like a bunch of nested dictioionaries)
-bus_data = json.loads(s12)
+bus_position_data = json.loads(vehicle_positions12)
+service_alert_data = json.loads(service_alerts35)
 
 #.keys loops throgh the dictionary keys and .items loops through what is in the keys
-
 
 #function to get all of the latitude and longitude coordinates of the busses with the number the user enters as a series of tuples inside of a list
 def get_bus_location(bus_name: str) -> list[tuple]:
 
     #list to store the the bus coordinates as
     list_of_busses = []
-
+    
     #loops through all of the entities
-    for item in bus_data.keys():
-
+    for item in bus_position_data.keys():
+        
         #if the key is for an entitiy (entities are bus entries)
         if item.__contains__('entity'):
 
             #if the currently selected bus matches the route number that the user has entered
-            if bus_data[item]["vehicle"]["trip"]["route_id"] == bus_name:
-
+            if bus_position_data[item]["vehicle"]["trip"]["route_id"] == bus_name:
+                
                 #add the coordinates to a tuple as a float
-                temporary_tuple = (bus_data[item]["id"], float(bus_data[item]["vehicle"]["position"]["latitude"]), float(bus_data[item]["vehicle"]["position"]["longitude"]))
+                temporary_tuple = (bus_position_data[item]["id"], float(bus_position_data[item]["vehicle"]["position"]["latitude"]), float(bus_position_data[item]["vehicle"]["position"]["longitude"]))
 
                 #add the tuple to the list 
                 list_of_busses.append(temporary_tuple)
@@ -104,32 +161,32 @@ def get_bus_location(bus_name: str) -> list[tuple]:
 #uses the bus ID to determine if it has wheelchair acceess
 def wheelchair_access (bus_id: str) -> bool:
 
-    for item in bus_data.keys():
+    for item in bus_position_data.keys():
 
         if item.__contains__('entity'):
 
-            if bus_data[item]["id"] == bus_id:
+            if bus_position_data[item]["id"] == bus_id:
 
-                if bus_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["wheelchair_accessible"] == "1":
+                if bus_position_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["wheelchair_accessible"] == "1":
                     return True
 
-                else if bus_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["wheelchair_accessible"] == "0":
+                elif bus_position_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["wheelchair_accessible"] == "0":
                     return False
                 
 
 #uses the bus ID to determine if it has air conditioning
 def air_conditioned (bus_id: str) -> bool:
 
-    for item in bus_data.keys():
+    for item in bus_position_data.keys():
         
         if item.__contains__('entity'):
             
-            if bus_data[item]["id"] == bus_id:
+            if bus_position_data[item]["id"] == bus_id:
                 
-                if bus_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["air_conditioned"] == "true":
+                if bus_position_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["air_conditioned"] == "true":
                     return True
 
-                else if bus_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["air_conditioned"] == "false":
+                elif bus_position_data[item]["vehicle"]["vehicle"]["transit_realtime.tfnsw_vehicle_descriptor"]["air_conditioned"] == "false":
                     return False
 
 
@@ -137,13 +194,13 @@ def air_conditioned (bus_id: str) -> bool:
 def bus_speed (bus_id: str) -> float:
 
     #loops through all of the entities
-    for item in bus_data.keys():
+    for item in bus_position_data.keys():
 
         if item.__contains__('entity'):
 
-            if bus_data[item]["id"] == bus_id:
+            if bus_position_data[item]["id"] == bus_id:
 
-                return float(bus_data[item]["vehicle"]["position"]["speed"])
+                return float(bus_position_data[item]["vehicle"]["position"]["speed"])
 
                     
     
@@ -151,7 +208,7 @@ def bus_speed (bus_id: str) -> float:
 while True:
     user_input = input("Type the bus that you would like the coordinates of: ")
 
-    print(get_bus_location(user_input)
+    print(get_bus_location(user_input))
 
     user_input2 = input("Again?")
 
@@ -159,10 +216,10 @@ while True:
         break
 
 '''
-for item in bus_data.keys():
+for item in bus_position_data.keys():
 
     if item.__contains__('entity'):
-        print(bus_data[item]["vehicle"]["trip"]["route_id"])
-        print("latitude: " + bus_data[item]["vehicle"]["position"]["latitude"])
-        print("longitude: " + bus_data[item]["vehicle"]["position"]["longitude"]+"\n")
+        print(bus_position_data[item]["vehicle"]["trip"]["route_id"])
+        print("latitude: " + bus_position_data[item]["vehicle"]["position"]["latitude"])
+        print("longitude: " + bus_position_data[item]["vehicle"]["position"]["longitude"]+"\n")
 '''
